0b0aa83f6ce2ac8634a23fe91f3d7e4b5446ca90
MomDra <yem9087@koreatech.ac.kr>
2021-11-16 23:00:31 +0900

    Update list view on upper region

--- TeamProject/app/src/main/java/com/example/teamproject/InfoActivity.java
+++ TeamProject/app/src/main/java/com/example/teamproject/InfoActivity.java
@@ -29,17 +29,28 @@ public class InfoActivity extends AppCompatActivity {
 
     public void RegionList(String region){
         ArrayList<String> destinations = new ArrayList<>();
-
-        if(region.equals("Jeolla")){
+        if(region.equals("Seoul")){
+            destinations.addAll(Arrays.asList(new String[]{"서울숲", "여행맛", "하늘공원"}));
+        }
+        else if(region.equals("Gyeonggi")){
+            destinations.addAll(Arrays.asList(new String[]{"대부도 바다향기 수목원", "평강랜드", "수산공원"}));
+        }
+        else if(region.equals("Chungcheong")){
+            destinations.addAll(Arrays.asList(new String[]{"구담봉", "옥순봉", "단양강 잔도", "제천 의림지와 제림", "예당호", "태안 빛축제", "웅도"}));
+        }
+        else if(region.equals("Gangwon")){
+            destinations.addAll(Arrays.asList(new String[]{"원대리 자작나무 숲", "태백산 천상의 화원", "상도문 돌담마을"}));
+        }
+        else if(region.equals("Jeolla")){
             destinations.addAll(Arrays.asList(new String[]{"강천산 단월야행", "선유도", "채석강", "황룡강", "순천만 습지", "화순 세량지"}));
         }
-        if(region.equals("Jeju")){
+        else if(region.equals("Jeju")){
             destinations.addAll(Arrays.asList(new String[]{"섭지코지", "우도", "9.81파크", "성산일출봉", "올레길", "만장굴"}));
         }
-        if(region.equals("Gyeongsang")){
+        else if(region.equals("Gyeongsang")){
             destinations.addAll(Arrays.asList(new String[]{"호미반도 해안둘레길", "국제 밤하늘 보호공원", "곤륜산 활공장", "보물섬 전망대", "바람의 언덕", "이순신 공원"}));
         }
-        if(region.equals("ETC")){
+        else if(region.equals("ETC")){
             destinations.addAll(Arrays.asList(new String[]{"인천 굴업도", "대전 상소동 산림욕장", "대구 달성공원", "광주 지산유원지", "부산 용소 웰빙공원", "울산 슬도"}));
         }
 
