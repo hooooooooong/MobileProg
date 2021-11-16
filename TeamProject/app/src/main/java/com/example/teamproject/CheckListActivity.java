c32df41bba4becabec910927f64b621e342e28af
dlgustn1245 <dlgustn1245@naver.com>
2021-11-16 22:50:01 +0900

    Add regional list views

--- TeamProject/app/src/main/java/com/example/teamproject/CheckListActivity.java
+++ TeamProject/app/src/main/java/com/example/teamproject/CheckListActivity.java
@@ -7,6 +7,7 @@ import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CheckListActivity extends AppCompatActivity {
     private ListView checkListView;
@@ -22,14 +23,13 @@ public class CheckListActivity extends AppCompatActivity {
     }
 
     public void CheckList(){
-        listItem = new ArrayList<String>();
         String[] checkList = {"결제 수단", "카메라", "보조배터리", "상비약", "멀티어댑터", "티슈", "옷",
                 "속옷", "잠옷", "신발", "양말", "수건", "화장품", "세면도구", "계절용품", "우산", "충전기", " 예약확인"};
 
-        for(int i=0;i<checkList.length;i++) listItem.add(checkList[i]);
+        ArrayList<String> listItem = new ArrayList<>(Arrays.asList(checkList));
 
         adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_multiple_choice, listItem);
-        checkListView = findViewById(R.id.listView1);
+        checkListView = findViewById(R.id.regionListView);
         checkListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         checkListView.setAdapter(adapter);
     }
--- TeamProject/app/src/main/java/com/example/teamproject/InfoActivity.java
+++ TeamProject/app/src/main/java/com/example/teamproject/InfoActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class InfoActivity extends AppCompatActivity {
     private ListView regionListView;
@@ -26,20 +28,23 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     public void RegionList(String region){
-        listItem = new ArrayList<String>();
-        switch (region){
-            case "Jeolla":
+        ArrayList<String> destinations = new ArrayList<>();
 
-                break;
-            case "Jeju":
-
-                break;
-            case "Gyeongsang":
-
-                break;
-            case "ETC": //이미지 바꿔야함
-
-                break;
+        if(region.equals("Jeolla")){
+            destinations.addAll(Arrays.asList(new String[]{"강천산 단월야행", "선유도", "채석강", "황룡강", "순천만 습지", "화순 세량지"}));
         }
+        if(region.equals("Jeju")){
+            destinations.addAll(Arrays.asList(new String[]{"섭지코지", "우도", "9.81파크", "성산일출봉", "올레길", "만장굴"}));
+        }
+        if(region.equals("Gyeongsang")){
+            destinations.addAll(Arrays.asList(new String[]{"호미반도 해안둘레길", "국제 밤하늘 보호공원", "곤륜산 활공장", "보물섬 전망대", "바람의 언덕", "이순신 공원"}));
+        }
+        if(region.equals("ETC")){
+            destinations.addAll(Arrays.asList(new String[]{"인천 굴업도", "대전 상소동 산림욕장", "대구 달성공원", "광주 지산유원지", "부산 용소 웰빙공원", "울산 슬도"}));
+        }
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, destinations);
+        regionListView = findViewById(R.id.regionListView);
+        regionListView.setAdapter(adapter);
    }
 }
