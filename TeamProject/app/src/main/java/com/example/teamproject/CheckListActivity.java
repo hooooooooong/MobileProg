d2e5434537ea8fc0db8d2847947a625306166d0a
MomDra <yem9087@koreatech.ac.kr>
2021-11-17 21:11:26 +0900

    Connect Activity

--- TeamProject/app/src/main/java/com/example/teamproject/CheckListActivity.java
+++ TeamProject/app/src/main/java/com/example/teamproject/CheckListActivity.java
@@ -1,10 +1,15 @@
 package com.example.teamproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.Carousel;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,7 +25,17 @@ public class CheckListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_checklist);
         CheckList();
 
+        AdapterView.OnItemClickListener a = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), adapter.getItem(i), Toast.LENGTH_SHORT).show();
+                checkListView.onSaveInstanceState();
+            }
+        };
+
+        checkListView.setOnItemClickListener(a);
     }
+
 
     public void CheckList(){
         String[] checkList = {"결제 수단", "카메라", "보조배터리", "상비약", "멀티어댑터", "티슈", "옷",
