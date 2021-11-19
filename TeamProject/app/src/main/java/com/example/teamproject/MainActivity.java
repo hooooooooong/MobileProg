e7bbe06ed85a83cdd9ecf2698cd7a2260701a244
js0403 <92702362+js0403@users.noreply.github.com>
2021-11-19 21:37:06 +0900

    테스트 주석 추가

--- TeamProject/app/src/main/java/com/example/teamproject/MainActivity.java
+++ TeamProject/app/src/main/java/com/example/teamproject/MainActivity.java
@@ -40,4 +40,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
     }
+
+    // Test
 }
